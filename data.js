cea3ee7d1a43ef0dbd37a64b56c98c8236246f56
Spuros <spurakos98@gmail.com>
2020-10-28 00:43:19 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,299 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Είσοδος",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.9563743425132945,
+        "pitch": 0.20394086806868295,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.14233977725722,
+          "pitch": 0.20706845288365372,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.000127332081629,
+          "pitch": 0.35934534311040345,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.0219501169294603,
+          "pitch": 0.12060720653415302,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.7140975226532564,
+          "pitch": 0.14188392086252577,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 3.0637150608304653,
+          "pitch": 0.15424864007948003,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Κουζίνα",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.874187874405145,
+        "pitch": 0.24982496733313475,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5678717540482392,
+          "pitch": 0.517318840557504,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.8953713903153933,
+          "pitch": 0.24801966020234012,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Διάδρομος",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.48166696375025,
+        "pitch": 0.29300611486623573,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8565908833070797,
+          "pitch": 0.278378475635936,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.945693168539753,
+          "pitch": 0.38416115118069527,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.5326500517532278,
+          "pitch": 0.5747164527631945,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.07104523458990464,
+          "pitch": 0.23748861655177222,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Σαλόνι",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.5398090421493773,
+        "pitch": 0.06666091740710733,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.726248987322334,
+          "pitch": 0.3044763325345059,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.7694914531337123,
+          "pitch": 0.04602487093560903,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Υπνοδωμάτιο",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.570080951523691,
+        "pitch": 0.15586468056850045,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.43610649865647133,
+          "pitch": 0.43131471516503517,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Μπάνιο",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.9692347383160698,
+        "pitch": 0.6107254964061308,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4250712515729616,
+          "pitch": 0.6782172334402841,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.3973232629727192,
+          "pitch": 0.21043457157120926,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "MyNextHome - Φιλίππου 93",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
